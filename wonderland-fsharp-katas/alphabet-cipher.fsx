253ba055b30aa646c7ed2a62c3e31ca152e6fe0d
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2015-10-24 19:21:06 -0700

    Setup and alphabet cipher
    Initial solution setup
    Added alphabet-cipher kata

---
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -0,0 +1,26 @@
+﻿type Message = string
+type Keyword = string
+
+let encode (key:Keyword) (message:Message) = "encodeme" : Message
+let decode (key:Keyword) (message:Message) = "decodeme" : Message
+let decipher (cipher:Message) (message:Message) = "decypherme" : Keyword
+
+#r @"packages\Unquote\lib\net45\Unquote.dll"
+open Swensen.Unquote
+
+let tests () =
+    
+    // verify encoding
+    test <@ encode "vigilance" "meetmeontuesdayeveningatseven" = "hmkbxebpxpmyllyrxiiqtoltfgzzv" @>
+    test <@ encode "scones" "meetmebythetree" = "egsgqwtahuiljgs" @>
+    
+    // verify decoding
+    test <@ decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv" = "meetmeontuesdayeveningatseven" @>
+    test <@ decode "scones" "egsgqwtahuiljgs" = "meetmebythetree" @>
+    
+    // verify decyphering
+    test <@ decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog" = "vigilance" @>
+    test <@ decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "packmyboxwithfivedozenliquorjugs" = "scones" @>
+
+// run the tests
+tests ()
