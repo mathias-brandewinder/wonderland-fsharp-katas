9b7dcfc97e925a0530c618b8cae425b5bcc8548d
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2015-10-30 23:17:40 -0700

    wonderland number
    Added wonderland number kata.

---
+++ wonderland-fsharp-katas/wonderland-number.fsx
@@ -0,0 +1,22 @@
+﻿let wonderlandNumber () = 42
+
+let haveSameDigits (n1:int,n2:int) =
+    (string n1 |> Set.ofSeq) = (string n2 |> Set.ofSeq)
+    
+#r @"../packages/Unquote/lib/net45/Unquote.dll"
+open Swensen.Unquote
+
+let tests () =
+    
+    let wonderNum = wonderlandNumber ()
+
+    test <@ (string wonderNum).Length = 6 @>
+
+    test <@ haveSameDigits (wonderNum, 2 * wonderNum) @>
+    test <@ haveSameDigits (wonderNum, 3 * wonderNum) @>
+    test <@ haveSameDigits (wonderNum, 4 * wonderNum) @>
+    test <@ haveSameDigits (wonderNum, 5 * wonderNum) @>
+    test <@ haveSameDigits (wonderNum, 6 * wonderNum) @>
+
+// run the tests
+tests ()
