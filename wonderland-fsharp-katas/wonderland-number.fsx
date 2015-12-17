5bee7fd0f348864b0c103473ca01d1bdf7b1b33a
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2015-12-16 17:28:32 -0800

    wonderland number solution
    Brute-force FTW!

--- wonderland-fsharp-katas/wonderland-number.fsx
+++ wonderland-fsharp-katas/wonderland-number.fsx
@@ -1,9 +1,20 @@
 // See the file wonderland-number.md for detailed information.
 
-let wonderlandNumber () = 42
-
 let haveSameDigits (n1:int,n2:int) =
     (string n1 |> Set.ofSeq) = (string n2 |> Set.ofSeq)
+
+let allGood (n:int) =
+    let multipliers = [ 2 .. 6 ]
+    (string n).Length = 6
+    && multipliers |> List.fold (fun ok mult -> ok && haveSameDigits (n, (n * mult))) true
+    
+// brute-force, baby, brute-force
+let wonderlandNumber () = 
+    let rec search x =
+        if allGood x 
+        then x
+        else search (x + 1)
+    search 100000
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
